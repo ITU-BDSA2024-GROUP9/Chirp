1147bac57d5aad8ce0707fb84eda890597302999
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-09-04 15:42:52 +0200

    Read cheeps

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -16,22 +16,12 @@ namespace Chirp.CLI
             {
                 try
                 {
-                    using StreamReader reader = new("./chirp_cli_db.csv");
-                    int i = 0;
-                    while (!reader.EndOfStream)
-                    {
-                        i++;
-                        string[]? text = (await reader.ReadLineAsync())?.Split(",\"");
-                        if (text == null || i == 1)
-                        {
-                            continue;
-                        }
-                        string[]? text2 = text[1].Split("\",");
-                        Cheep cheep = new Cheep(text[0],text2[0],long.Parse(text2[1]));
-                        UserInterface.ShowCheep(cheep);
-                    }
+                    CSVDatabaseService<Cheep> csv = new();
+                    IAsyncEnumerable<Cheep> list = await csv.ReadAsync(1);
+                    await foreach(Cheep c in list)
+                        UserInterface.ShowCheep(c);
                 }
-                catch (IOException e)
+                catch (Exception e)
                 {
                     Console.WriteLine("The file could not be read:");
                     Console.WriteLine(e.Message);
