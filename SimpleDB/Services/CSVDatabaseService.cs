2834504826389269f25b09e4925ab5ebf49e80f7
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-09-04 14:41:28 +0200

    Created CSV database service and SQLlite database servic

---
+++ SimpleDB/Services/CSVDatabaseService.cs
@@ -0,0 +1,22 @@
+﻿using SimpleDB.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDB.Services
+{
+    public class CSVDatabaseService<T> : IDatabaseRepository<T>
+    {
+        Task<IEnumerable<T>> IDatabaseRepository<T>.ReadAsync(int? count)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IDatabaseRepository<T>.StoreAsync(T record)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
