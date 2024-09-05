0adee9ea1ef1c2c5922abf6bc67c36e586801d17
Phillip <phir@itu.dk>
2024-09-05 21:15:17 +0200

    Fixed a bug when reading and writing to file. You can now read and write to the CSV file

--- SimpleDB/Services/CSVDatabaseService.cs
+++ SimpleDB/Services/CSVDatabaseService.cs
@@ -9,23 +9,36 @@ using System.Threading.Tasks;
 using CsvHelper;
 using Chirp.Core.Classes;
 using System.Reflection;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace SimpleDB.Services
 {
     public class CSVDatabaseService<T> : IDatabaseRepository<T> where T : IPost
     {
+        private string getFilePath()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var projectDirectory = Path.Combine(currentDirectory, "..", "SimpleDB", "Data");
+            var test = Path.Combine(projectDirectory, "chirp_cli_db.csv");
+            Console.WriteLine(test);
+            return test;
+        }
         public IEnumerable<T> Read(int? count)
         {
-            using (var reader = new StreamReader(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data/chirp_cli_db.csv")))
+            using (var reader = new StreamReader(getFilePath()))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
                 return csv.GetRecords<T>().ToList<T>();
         }
 
         public void Store(T record)
         {
-            using (var writer = new StreamWriter(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data/chirp_cli_db.csv")))
+            using (var writer = new StreamWriter(getFilePath(), true))
             using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.NextRecord(); // Next line in the CSV file
                 csv.WriteRecord<T>(record);
+            }
+                
         }
     }
 }
