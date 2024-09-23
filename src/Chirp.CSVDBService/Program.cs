c5bb39b0b21c64f402d9d597b803f42b7c821200
JKlarlund <jonathanklarlund@outlook.com>
2024-09-23 18:31:30 +0200

    Save cheeps when running the cheep command

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,19 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Cheep> x = new List<Cheep>();
+x?.Add(new Cheep("me1", "Hej!", 1684219348));
+x?.Add(new Cheep("me", "Hej!222", 1684229348));
+x?.Add(new Cheep("me2", "Hejzs!", 1684223348));
 
 List<Cheep> get()
 {
-    List<Cheep> x = new List<Cheep>();
-    x?.Add(new Cheep("me1", "Hej!", 1684219348));
-    x?.Add(new Cheep("me", "Hej!222", 1684229348));
-    x?.Add(new Cheep("me2", "Hejzs!", 1684223348));
     return x;
 }
 
 
 app.MapGet("/cheeps", () => get());
-app.MapPost("/cheep", (Cheep cheep) => { Console.WriteLine("recieved: " + cheep.Message); });
+app.MapPost("/cheep", (Cheep cheep) =>
+{
+    x.Add(cheep);
+    Console.WriteLine("recieved: " + cheep.Message); 
+});
 
 app.Run();
 
