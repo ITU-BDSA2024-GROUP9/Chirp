71a4c75487e0c17ad578ca18bc7153afcc90de6a
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-09-23 15:37:37 +0200

    Post List of Cheep

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,7 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/cheeps", () => new Cheep("me", "Hej!", 1684229348));
+
+List<Cheep> get()
+{
+    var x = new List<Cheep>();
+    x.Add(new Cheep("me", "Hej!", 1684229348));
+    return x;
+}
+
+
+app.MapGet("/cheeps", () => get());
 app.MapPost("/cheep", (Cheep cheep) => { Console.WriteLine("recieved: " + cheep.Message); });
 
 app.Run();
