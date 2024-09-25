bd2d42087fd244c2ba6c885fd53e41df8f920e7b
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-09-25 14:54:12 +0200

    Using SQLiteConnection

--- src/Chirp.Core/Helpers/DBFacade.cs
+++ src/Chirp.Core/Helpers/DBFacade.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace Chirp.Core.Helpers
 {
     public class DBFacade
     {
+        public void test()
+        {
+            var sqlDBFilePath = "/tmp/chirp.db";
+            var sqlQuery = @"SELECT * FROM message ORDER by message.pub_date desc";
+
+            using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = sqlQuery;
+
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                 
+                }
+            }
+        }
     }
 }
