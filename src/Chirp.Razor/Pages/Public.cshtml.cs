2aab926a20daa4a435521ba0b4481c022e77d1f1
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-09-25 14:42:26 +0200

    Fix using import

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Chirp.Core.Classes;
+using Chirp.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
