59ef15a2185a046cb3caa87e57a6f60248b744e4
JKlarlund <jonathanklarlund@outlook.com>
2024-11-13 16:45:39 +0100

    Follow/unfollow based on if author is already followed or not Co-authored-by: Phillip <phir@itu.dk> Co-authored-by: Jonathan9267 <jork@itu.dk>

--- src/Chirp.Razor/Pages/Model.cs
+++ src/Chirp.Razor/Pages/Model.cs
@@ -53,11 +53,27 @@ public class Model : PageModel
         TotalPages = PageAmount(_service.GetCheepByName(authorName));
         Cheeps = _service.GetCheepsFromAuthorByName(authorName, queryPage);
     }
-    
+
+    public string getAuthorID(string authorName)
+    {
+        return _service.GetAuthorByName(authorName).Id;
+    }
+
     private int PageAmount(int totalCheeps)
     {
         int pages = (int) Math.Ceiling(1.0 * totalCheeps / 32);
         return pages <= 0 ? 1 : pages;
+    }
+
+    public string isFollower(string userid, string author_userid)
+    {
+        /**
+        if (_service.GetAuthorByID("userid").Following.Contains(author_userid))
+        {
+            return "Unfollow";
+        }
+        **/
+        return "Follow";
     }
 
     public IActionResult OnPostCreateCheep()
