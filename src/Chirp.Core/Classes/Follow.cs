a05036425e6a2d6cbcd1663af972ca4448a03902
DefaultDaddy <philliprasmussen2406@gmail.com>
2024-11-13 17:37:49 +0100

    Follow button is only displayed for users who are logged in and the follow button does not appear on own cheeps Co-authored-by: Jonathan9267 <jork@itu.dk>

--- src/Chirp.Core/Classes/Follow.cs
+++ src/Chirp.Core/Classes/Follow.cs
@@ -2,9 +2,9 @@ namespace Chirp.Core.Classes;
 
 public class Follow
 {
-    public string FollowerId { get; set; }
-    public Author Follower { get; set; }
+    public string? FollowerId { get; set; }
+    public Author? Follower { get; set; }
         
-    public string FollowedId { get; set; }
-    public Author Followed { get; set; }
+    public string? FollowedId { get; set; }
+    public Author? Followed { get; set; }
 }
