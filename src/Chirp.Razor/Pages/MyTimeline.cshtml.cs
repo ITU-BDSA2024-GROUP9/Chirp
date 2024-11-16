9fb06aa257a8327bae8ae1de124c934a0ffeedb9
Phillip <phir@itu.dk>
2024-11-16 10:53:54 -0500

    The pages now work again!

--- src/Chirp.Razor/Pages/MyTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/MyTimeline.cshtml.cs
@@ -1,6 +1,7 @@
-
+using Chirp.Core.Classes;
 using Chirp.Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
@@ -10,9 +11,16 @@ public class MyTimelineModel : Model
 
     public ActionResult OnGet([FromQuery] int page, string author)
     {
-        if (page < 1) page = 1;
-        base.PaginateCheepsByFollowers(page, author);
+        // Default to the current user's name if `author` is not provided
+        if (string.IsNullOrEmpty(author))
+        {
+            author = User.Identity?.Name;
+            Console.WriteLine("Defaulted author to User.Identity.Name: " + author);
+        }
         
+        if (!string.IsNullOrEmpty(author))
+            base.PaginateCheepsByName(page, author);
+
         return Page();
     }
 
