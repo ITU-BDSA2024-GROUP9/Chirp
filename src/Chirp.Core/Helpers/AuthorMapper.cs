2a95418b7c4c831342f0981329aabe6d807fd2f1
Phillip <phir@itu.dk>
2024-12-04 11:11:29 +0100

    Created AuthorMapper.cs to reduce the code needed for converting an Author to and from AuthorDTO. This massively saves a lot of duplicated code and also makes it easier to maintain/add new features.

---
+++ src/Chirp.Core/Helpers/AuthorMapper.cs
@@ -0,0 +1,34 @@
+using Chirp.Core.Classes;
+
+namespace Chirp.Core.Helpers;
+
+public class AuthorMapper
+{
+    public static AuthorDTO toDTO(Author author)
+    {
+        return new AuthorDTO
+        {
+            Id = author.Id,
+            UserName = author.UserName,
+            Email = author.Email,
+            Cheeps = author.Cheeps,
+            Following = author.Following,
+            Followers = author.Followers,
+            Comments = author.Comments
+        };
+    }
+
+    public static Author toDomain(AuthorDTO authorDTO)
+    {
+        return new Author
+        {
+            Id = authorDTO.Id,
+            UserName = authorDTO.UserName,
+            Email = authorDTO.Email,
+            Cheeps = authorDTO.Cheeps,
+            Following = authorDTO.Following,
+            Followers = authorDTO.Followers,
+            Comments = authorDTO.Comments
+        };
+    }
+}
