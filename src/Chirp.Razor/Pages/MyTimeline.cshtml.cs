97fcb89b060d53cff60c2799dc9c1b7ae0f332ac
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-12-05 01:35:28 +0100

    merge

--- src/Chirp.Razor/Pages/MyTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/MyTimeline.cshtml.cs
@@ -1,31 +1,29 @@
-using Chirp.Core.Classes;
-using Chirp.Core.Interfaces;
+using Chirp.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
 public class MyTimelineModel : Model
 {
-    public MyTimelineModel(ICheepService service) : base(service) { }
+	public MyTimelineModel(ICheepService service) : base(service) { }
 
-    public ActionResult OnGet([FromQuery] int page, string? author)
-    {
-        if (page < 1) page = 1;
-        // Default to the current user's name if `author` is not provided
-        if (string.IsNullOrEmpty(author))
-        {
-            author = User.Identity?.Name;
-        }
-        
-        if (!string.IsNullOrEmpty(author))
-            PaginateCheepsByFollowers(page, author);
+	public ActionResult OnGet([FromQuery] int page, string? author)
+	{
+		if (page < 1) page = 1;
+		// Default to the current user's name if `author` is not provided
+		if (string.IsNullOrEmpty(author))
+		{
+			author = User.Identity?.Name;
+		}
 
-        return Page();
-    }
+		if (!string.IsNullOrEmpty(author))
+			PaginateCheepsByFollowers(page, author);
 
-    public new IActionResult OnPostDeleteCheep(int cheepId, int page = 1)
-    {
-        return base.OnPostDeleteCheep(cheepId, page);
-    }
+		return Page();
+	}
+
+	public new IActionResult OnPostDeleteCheep(int cheepId, int page = 1)
+	{
+		return base.OnPostDeleteCheep(cheepId, page);
+	}
 }
