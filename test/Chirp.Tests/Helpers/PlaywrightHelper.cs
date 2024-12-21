823f8c96fa0a984ec2f7619c59965caa170c6cbe
Jonathan Rønnow Klarlund <jonathanklarlund@outlook.com>
2024-12-21 20:03:22 +0100

    Adding Login method to Playwrighthelper class.

---
+++ test/Chirp.Tests/Helpers/PlaywrightHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+
+namespace Chirp.Tests.Helpers;
+
+public static class PlaywrightHelper
+{
+    public static async Task LoginAsync(IPage page, string userName, string password)
+    {
+        await page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await page.GetByPlaceholder("name@example.com").FillAsync(userName);
+        await page.GetByPlaceholder("password").FillAsync(password);
+        await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+    }
+}
