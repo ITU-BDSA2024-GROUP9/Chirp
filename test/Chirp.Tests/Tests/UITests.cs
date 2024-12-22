ca20b38cecb497cc839652528338edbb1d9403be
Jonathan Rønnow Klarlund <jonathanklarlund@outlook.com>
2024-12-22 20:15:56 +0100

    Fix UserRegistersAndPostsCheepAndAccessesPrivateTimeline

--- test/Chirp.Tests/Tests/UITests.cs
+++ test/Chirp.Tests/Tests/UITests.cs
@@ -64,18 +64,14 @@ namespace Chirp.Tests.Tests
 			var userName = "test";
 			var email = userName + "@mail.com";
 			var password = "Test1!";
+			var cheepMessage = "Hello";
 			await PlaywrightHelper.RegisterAsync(Page, userName, email, password);
 			await PlaywrightHelper.LoginAsync(Page, userName, password);
-			
-			await Page.GetByPlaceholder("Type here!").ClickAsync();
-			await Page.GetByPlaceholder("Type here!").FillAsync("Hi everyone!");
-			await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
-			await Page.GetByRole(AriaRole.Link, new() { Name = "My Timeline" }).ClickAsync();
-			await Page.GetByText("test@mail.com Hi everyone! —").ClickAsync();
+			await PlaywrightHelper.Cheep(Page, cheepMessage);
+			await PlaywrightHelper.AccessOwnTimeline(Page);
+			NUnit.Framework.Assert.NotNull(Page.GetByText(userName + cheepMessage + " Just now", new() {Exact = true}));
 		}
-
-
-
+		
 		[TearDown]
 		public void Cleanup()
 		{
