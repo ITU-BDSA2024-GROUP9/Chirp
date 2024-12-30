859a7b969d84349c461d67cb08b6bfc1a9cea336
Jonathan Rønnow Klarlund <jonathanklarlund@outlook.com>
2024-12-30 10:30:47 +0100

    Setup pages and stop server at the end of all tests.

--- test/Chirp.Tests/Tests/UITests.cs
+++ test/Chirp.Tests/Tests/UITests.cs
@@ -8,62 +8,72 @@ using helpers = Chirp.Tests.Helpers;
 
 namespace Chirp.Tests.Tests
 {
-	[Parallelizable(ParallelScope.Self)]
+	[NonParallelizable]
 	[TestFixture]
 	public class UITests : PageTest
 	{
+		
+		private IPage _page;
+		private IBrowser _browser;
 		[SetUp]
 		public async Task Init()
 		{ 
-			await ServerHelper.StartServer(); // Starts the server before each test
-			Page.SetDefaultTimeout(12000); //Reduce the default timeout
+			//await ServerHelper.StartServer(); // Starts the server before each test
+			_browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+			_page = await _browser.NewPageAsync();
+			_page.SetDefaultTimeout(6000); //Reduce the default timeout
 		}
 
 		[Test]
+		[NonParallelizable]
 		[TestCase("uitester1", "uitester1@mail.com", "Test1!")]
 		public async Task TestRegisterAndLoginAndLogout(string username, string email, string password)
 		{
-			await Page.GotoAsync("http://localhost:5273/");
-			await PlaywrightHelper.RegisterAsync(Page, username, email, password);
-			NUnit.Framework.Assert.True(Page.IsVisibleAsync("text = " + username).Result);
-			await PlaywrightHelper.LogoutAsync(Page, username);
-			NUnit.Framework.Assert.False(Page.IsVisibleAsync("text = " + username).Result);
+			await _page.GotoAsync("http://localhost:5273/");
+			await PlaywrightHelper.RegisterAsync(_page, username, email, password);
+			NUnit.Framework.Assert.True(_page.IsVisibleAsync("text = " + username).Result);
+			await PlaywrightHelper.LogoutAsync(_page, username);
+			NUnit.Framework.Assert.False(_page.IsVisibleAsync("text = " + username).Result);
 		}
-
 		[Test]
+		[NonParallelizable]
 		public async Task TestAccessUserTimelineAsAnonymousUser()
 		{
-			await Page.GotoAsync("http://localhost:5273/");
-			string? cheepText = Page.Locator("li").First.TextContentAsync().Result;
+			await _page.GotoAsync("http://localhost:5273/");
+			string? cheepText = _page.Locator("li").First.TextContentAsync().Result;
 			string? authorName = cheepText.Split(" ·")[0].Trim();
-			await Page.Locator("li").First.GetByRole(AriaRole.Link).ClickAsync();
-			NUnit.Framework.Assert.True(Page.IsVisibleAsync("text = " + authorName + "'s Timeline").Result);
+			await _page.Locator("li").First.GetByRole(AriaRole.Link).ClickAsync();
+			NUnit.Framework.Assert.True(_page.IsVisibleAsync("text = " + authorName + "'s Timeline").Result);
 		}
-
 		[Test]
+		[NonParallelizable]
 		[TestCase("uitester2", "uitester2@mail.com", "Test1!")]
 		public async Task UserRegistersAndAccessesUserTimeline(string username, string email, string password)
 		{
-			await Page.GotoAsync("http://localhost:5273/");
-			await PlaywrightHelper.RegisterAsync(Page, username, email, password);
-			var cheepContent = PlaywrightHelper.GetAuthorOfMostRecentCheep(Page);
-			await Page.Locator("li").First.GetByRole(AriaRole.Link).ClickAsync(); //Clicks on the author and should therefore be redirected to author's timeline.
-			NUnit.Framework.Assert.True(Page.IsVisibleAsync("text = " + cheepContent + "'s Timeline").Result);
+			await _page.GotoAsync("http://localhost:5273/");
+			await PlaywrightHelper.RegisterAsync(_page, username, email, password);
+			var cheepContent = PlaywrightHelper.GetAuthorOfMostRecentCheep(_page);
+			await _page.Locator("li").First.GetByRole(AriaRole.Link).ClickAsync(); //Clicks on the author and should therefore be redirected to author's timeline.
+			NUnit.Framework.Assert.True(_page.IsVisibleAsync("text = " + cheepContent + "'s Timeline").Result);
 		}
-
 		[Test]
+		[NonParallelizable]
 		[TestCase("uitester3", "uitester3@mail.com", "Test1!", "Hello!")]
 		public async Task UserRegistersAndPostsCheepAndAccessesPrivateTimeline(string username, string email, string password, string cheepMessage)
 		{
-			await Page.GotoAsync("http://localhost:5273/");
-			await PlaywrightHelper.RegisterAsync(Page, username, email, password);
-			await PlaywrightHelper.Cheep(Page, cheepMessage);
-			await PlaywrightHelper.AccessOwnTimeline(Page);
-			NUnit.Framework.Assert.NotNull(Page.GetByText(username + cheepMessage + " Just now", new() {Exact = true}));
+			await _page.GotoAsync("http://localhost:5273/");
+			await PlaywrightHelper.RegisterAsync(_page, username, email, password);
+			await PlaywrightHelper.Cheep(_page, cheepMessage);
+			await PlaywrightHelper.AccessOwnTimeline(_page);
+			NUnit.Framework.Assert.NotNull(_page.GetByText(username + cheepMessage + " Just now", new() {Exact = true}));
 		}
-		
 		[TearDown]
-		public void Cleanup()
+		public async Task Cleanup()
+		{
+			await PlaywrightHelper.TearDown(_page, _browser);
+		}
+		[OneTimeTearDown]
+		public void StopServer()
 		{
 			ServerHelper.StopServer(); // Stops the server after each test
 		}
